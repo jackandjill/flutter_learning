66b30550b1df69c785f7535189d756ccbd6c8297
OREODSK\bhara <ybpriya@gmail.com>
2018-06-04 16:24:52 +0800

    Able to see tb's in NewItem

--- lib/screens/NewItem.dart
+++ lib/screens/NewItem.dart
@@ -21,128 +21,142 @@ class NewItemCreateState extends State<NewItemCreate>
           elevation: 5.0,
         ),
       body:
+      Center  (
+          child: ListView(
+            padding: EdgeInsets.only(left: 24.0, right: 24.0),
+            shrinkWrap: true,
+            children: <Widget>[
 
-
-          new Center(
-            child:   Container(
-              color: Colors.blue,
-
-        padding: new EdgeInsets.fromLTRB(8.0,10.0,0.0,0.0),
-
-        child: Column(
-
-
-          children: <Widget>[
-         Text(
-        "NEW",
-          textScaleFactor: 5.0,
-        ),
-          ],
-        )
-      )
+              itemName,
+              SizedBox(height: 18.0),
+              itemLocation,
+              SizedBox(height: 18.0),
+              itemLocation,
+              SizedBox(height: 18.0),
+              itemTime,
+              SizedBox(height: 18.0),
+              Report,
+              SizedBox(height: 38.0),
+            ],
           )
+      )
 
-      /*
-      GridView.count(
-        shrinkWrap: true,
-        crossAxisCount: 2, //number of columns
-        children: <Widget>[
-          Card(
-            child: Column(
-              // mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                  child: Stack(
-                    fit: StackFit.expand,
-                    children: <Widget>[
-                      Container(
-                        height: MediaQuery.of(context).size.height / 4,
-                        width: MediaQuery.of(context).size.height / 2.5,
-                        child: DecoratedBox(
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                                image: NetworkImage(
-                                    "https://i.imgur.com/FtaGNck.jpg"),
-                                fit: BoxFit.cover),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Align(
-                          alignment: FractionalOffset.topLeft,
-                          child: CircleAvatar(
-                            backgroundColor: Colors.redAccent,
-                            radius: 15.0,
-                            child: Text(
-                              "NEW",
-                              textScaleFactor: 0.5,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Align(
-                        alignment: FractionalOffset.bottomRight,
-                        child: Container(
-                          color: Colors.blueAccent,
-                          height: 35.0,
-                          width: 35.0,
-                          child: Center(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Icon(Icons.account_circle),
-                                Text(
-                                  "1P",
-                                  textScaleFactor: 0.5,
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Center(
-                  child: Container(
-                    padding: const EdgeInsets.all(8.0),
-                    alignment: FractionalOffset.bottomCenter,
-                    child: Text(
-                      "AWESOME DISH",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    FlatButton(
-                      child: Text(
-                        "Add To Cart",
-                        style: TextStyle(color: Colors.grey[500]),
-                      ),
-                      onPressed: () => null,
-                    ),
-                    Text(
-                      "\$5",
-                      style: TextStyle(color: Colors.grey[500]),
-                    )
-                  ],
-                )
-              ],
-            ),
-          ),
-        ],
-      ), */
+
+
     );
 
 
 
   }
+
+
+
+
+  final itemName = TextFormField(
+    keyboardType: TextInputType.text,
+    autofocus: false,
+    initialValue: 'iPhone 6s',
+    decoration: InputDecoration(
+      hintText: 'Item Name',
+      contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+
+      border: OutlineInputBorder(
+
+          borderRadius: BorderRadius.circular(32.0)
+      ),
+
+    ),
+  );
+
+  final itemDescription = TextFormField(
+    keyboardType: TextInputType.text,
+    autofocus: false,
+    initialValue: 'iPhone 6 rose color with wooden casing',
+    decoration: InputDecoration(
+      hintText: 'Describe item briefly.',
+      contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+
+      border: OutlineInputBorder(
+
+          borderRadius: BorderRadius.circular(32.0)
+      ),
+
+    ),
+
+  );
+
+  final itemLocation = TextFormField(
+    keyboardType: TextInputType.text,
+    autofocus: false,
+    initialValue: 'Hougang',
+    decoration: InputDecoration(
+      hintText: 'Where did you find it?',
+      contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+      border: OutlineInputBorder(
+
+          borderRadius: BorderRadius.circular(32.0)
+      ),
+
+    ),
+
+  );
+
+  final itemTime = TextFormField(
+    keyboardType: TextInputType.datetime,
+    obscureText: true,
+    autofocus: false,
+    //initialValue: '10.20',
+    decoration: InputDecoration(
+      hintText: 'When you found it?',
+      contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+      border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(32.0)
+      ),
+
+    ),
+
+  );
+
+  
+
+  final Report = Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        borderRadius: BorderRadius.circular(30.0),
+        shadowColor: Colors.lightBlueAccent.shade100,
+        elevation: 5.0,
+        child: MaterialButton(
+          minWidth: 200.0,
+          height: 42.0,
+          onPressed: (){
+            print("Signup button clicked");
+          },
+          color: Colors.lightBlueAccent,
+          child:
+          Text('Sign Up',
+            style: TextStyle(color: Colors.white, fontSize: 20.0),
+          ),
+        ),
+      )
+  );
+
+
+
+  final phonenumber = TextFormField(
+    keyboardType: TextInputType.phone,
+    autofocus: false,
+    initialValue: '91166666',
+    decoration: InputDecoration(
+      hintText: 'Phone Number',
+      contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+      border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(32.0)
+      ),
+
+    ),
+
+  );
+
 }
 
 Widget _buildNewItem ()
